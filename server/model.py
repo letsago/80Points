a01d53eb5c6263a8103176230a020a94802d63e5
Patrick Yu <pa.yu@hotmail.com>
2019-02-19 18:28:09 -0800

    updated suit_power to handle nonconsecutive pairs and joker trump rounds updated cards_to_tractors function to only merge rank > 1 tractors

--- server/model.py
+++ server/model.py
@@ -57,18 +57,28 @@ class Card(object):
 		  match the trump suit are assigned the same power. (For example, if trump
 		  is 2C, 2D and 2S have the same power.)
 		'''
+
 		if not self.is_trump(trump_card):
+			# handles power level cases like 2,2,4,4 being tractor if 3 is trump value
+			if CARD_VALUES.index(self.value) > CARD_VALUES.index(trump_card.value):
+				return CARD_VALUES.index(self.value) - 1
 			return CARD_VALUES.index(self.value)
 
 		if self.suit == 'joker' and self.value == 'big':
-			return len(CARD_VALUES) + 3
-		elif self.suit == 'joker' and self.value == 'small':
 			return len(CARD_VALUES) + 2
-		elif self.suit == trump_card.suit and self.value == trump_card.value:
+		elif self.suit == 'joker' and self.value == 'small':
 			return len(CARD_VALUES) + 1
-		elif self.value == trump_card.value:
+		# handles case where joker is trump suit and all trump values are at equal power level
+		elif trump_card.suit == 'joker' and self.value == trump_card.value:
+			return len(CARD_VALUES)	
+		elif self.suit == trump_card.suit and self.value == trump_card.value:
 			return len(CARD_VALUES)
+		elif self.value == trump_card.value:
+			return len(CARD_VALUES) - 1
 		else:
+			# handles power level cases like 2,2,4,4 being tractor if 3 is trump value
+			if CARD_VALUES.index(self.value) > CARD_VALUES.index(trump_card.value):
+				return CARD_VALUES.index(self.value) - 1
 			return CARD_VALUES.index(self.value)
 
 	def display_index(self, trump_card):
--- server/tractor_test.py
+++ server/tractor_test.py
@@ -14,47 +14,143 @@ def Straight(suit, value, length):
 	return Many(suit, value, length=length)
 
 def Double(suit, value):
+	if suit == 'joker':
+		return 2 * [Card(suit, value)]
 	return Many(suit, value, rank=2)
 
 class TestTractor(unittest.TestCase):
-	def testCardsToTractors(self):
-		trump_card = Card('s', 'A')
+	def testNonJokerTrumpCardsToTractors(self):
+		trump_card = Card('s', '8')
+		trick_suit = 's'
 		tests = [
+			# consecutive pairs (s 2, s 2, s 3, s 3)
 			(Double('s', '2') + Double('s', '3'),
 			 [Tractor(2, 2, Card('s', '2').suit_power(trump_card), SUIT_TRUMP)]),
 
+			# consecutive singles (s 2, s 3, s 4, s 5)
 			(Straight('s', '2', 4),
-			 [Tractor(1, 4, Card('s', '2').suit_power(trump_card), SUIT_TRUMP)]),
+			 [
+				Tractor(1, 1, Card('s', '5').suit_power(trump_card), SUIT_TRUMP),
+			 	Tractor(1, 1, Card('s', '4').suit_power(trump_card), SUIT_TRUMP),
+			 	Tractor(1, 1, Card('s', '3').suit_power(trump_card), SUIT_TRUMP),
+			 	Tractor(1, 1, Card('s', '2').suit_power(trump_card), SUIT_TRUMP),
+			 ]),
 
+			# single card (s 2)
 			([Card('s', '2')],
 			 [Tractor(1, 1, Card('s', '2').suit_power(trump_card), SUIT_TRUMP)]),
 
+			# consecutive pairs (s 2, s 2, s 3, s 3) and single card (s 5)
 			(Double('s', '2') + Double('s', '3') + [Card('s', '5')],
 			 [
 				Tractor(2, 2, Card('s', '2').suit_power(trump_card), SUIT_TRUMP),
 				Tractor(1, 1, Card('s', '5').suit_power(trump_card), SUIT_TRUMP),
 			 ]),
 
+			# pair (s 2, s 2) and single card (s 3)
 			(Double('s', '2') + [Card('s', '3')],
 			 [
 				Tractor(2, 1, Card('s', '2').suit_power(trump_card), SUIT_TRUMP),
 				Tractor(1, 1, Card('s', '3').suit_power(trump_card), SUIT_TRUMP),
 			 ]),
 
-			(Double('s', '2') + Double('s', '3') + [Card('s', '4')],
+			# consecutive pairs (s 2, s 2, s 3, s 3, s 4, s 4)
+			(Double('s', '2') + Double('s', '3') + Double('s', '4'),
+			 [Tractor(2, 3, Card('s', '2').suit_power(trump_card), SUIT_TRUMP)]),
+
+			# non-consecutive pairs (s 2, s 2, s 5, s 5, s 9, s 9)
+			(Double('s', '2') + Double('s', '5') + Double('s', '9'),
 			 [
-				Tractor(2, 2, Card('s', '2').suit_power(trump_card), SUIT_TRUMP),
-				Tractor(1, 1, Card('s', '4').suit_power(trump_card), SUIT_TRUMP),
+				Tractor(2, 1, Card('s', '9').suit_power(trump_card), SUIT_TRUMP),
+				Tractor(2, 1, Card('s', '5').suit_power(trump_card), SUIT_TRUMP),
+				Tractor(2, 1, Card('s', '2').suit_power(trump_card), SUIT_TRUMP),
 			 ]),
 
-			# (s A) is high trump A while (d A) is low trump A
-			(Double('s', 'A') + Double('d', 'A'),
-			 [Tractor(2, 2, Card('d', 'A').suit_power(trump_card), SUIT_TRUMP)]),
+			# consecutive pairs (s 2, s 2, s 3, s 3), pair (s 9, s 9), single card (big joker) 
+			(Double('s', '2') + Double('s', '3') + Double('s', '9') + [Card('joker', 'big')],
+			 [
+				Tractor(2, 1, Card('s', '9').suit_power(trump_card), SUIT_TRUMP),
+				Tractor(2, 2, Card('s', '2').suit_power(trump_card), SUIT_TRUMP),
+				Tractor(1, 1, Card('joker', 'big').suit_power(trump_card), SUIT_TRUMP),
+			 ]),
+
+			# consecutive pairs (h 7, h 7, h 9, h 9) since (s 8) is trump
+			(Double('h', '7') + Double('h', '9'),
+			 [Tractor(2, 2, Card('s', '7').suit_power(trump_card), SUIT_LOWEST)]),
+
+			# consecutive pairs (d 8, d 8, s 8, s 8) since (s 8) is high trump 8 and (d 8) is low trump 8
+			(Double('s', '8') + Double('d', '8'),
+			 [Tractor(2, 2, Card('d', '8').suit_power(trump_card), SUIT_TRUMP)]),
+
+			# consecutive pairs (s A, s A, h 8, h 8) since (s 8) is trump
+			(Double('s', 'A') + Double('h', '8'),
+			 [Tractor(2, 2, Card('s', 'A').suit_power(trump_card), SUIT_TRUMP)]),
+
+			# non-consecutive pairs (c 8, c 8, h 8, h 8) since (s 8) is trump
+			(Double('c', '8') + Double('h', '8'),
+			 [
+				Tractor(2, 1, Card('c', '8').suit_power(trump_card), SUIT_TRUMP),				 
+				Tractor(2, 1, Card('h', '8').suit_power(trump_card), SUIT_TRUMP),
+			 ]),
+
+			# consecutive pairs (s 8, s 8, small joker, small joker) since (s 8) is trump
+			(Double('s', '8') + Double('joker', 'small'),
+			 [
+				Tractor(2, 2, Card('s', '8').suit_power(trump_card), SUIT_TRUMP),
+			 ]),
+
+			# consecutive pairs (small joker, small joker, big joker, big joker)
+			(Double('joker', 'big') + Double('joker', 'small'),
+			 [
+				Tractor(2, 2, Card('joker', 'small').suit_power(trump_card), SUIT_TRUMP),				 
+			 ])
 		]
 
 		for test in tests:
 			test_cards, want = test
-			self.assertEqual(cards_to_tractors(test_cards, 's', trump_card), want)
+			self.assertEqual(cards_to_tractors(test_cards, trick_suit, trump_card), want)
+	
+	def testJokerTrumpCardsToTractors(self):
+		# Card('joker', '8') object represents the trump suit and value, not an actual card
+		trump_card = Card('joker', '8')
+		trick_suit = 's'
+	
+		tests = [
+			# non-consecutive pairs (c 8, c 8, h 8, h 8) since (joker, 8) is trump
+			(Double('c', '8') + Double('h', '8'),
+			 [
+				Tractor(2, 1, Card('c', '8').suit_power(trump_card), SUIT_TRUMP),				 
+				Tractor(2, 1, Card('h', '8').suit_power(trump_card), SUIT_TRUMP),
+			 ]),
+
+			# consecutive pairs (c 8, c 8, small joker, small joker) since (joker, 8) is trump
+			(Double('c', '8') + Double('joker', 'small'),
+			 [
+				Tractor(2, 2, Card('c', '8').suit_power(trump_card), SUIT_TRUMP),				 
+			 ]),
+
+			# consecutive pairs (d 8, d 8, small joker, small joker) since (joker, 8) is trump
+			(Double('d', '8') + Double('joker', 'small'),
+			 [
+				Tractor(2, 2, Card('d', '8').suit_power(trump_card), SUIT_TRUMP),				 
+			 ]),
+			
+			# consecutive pairs (h 8, h 8, small joker, small joker) since (joker, 8) is trump
+			(Double('h', '8') + Double('joker', 'small'),
+			 [
+				Tractor(2, 2, Card('h', '8').suit_power(trump_card), SUIT_TRUMP),				 
+			 ]),
+
+			# consecutive pairs (s 8, s 8, small joker, small joker) since (joker, 8) is trump
+			(Double('s', '8') + Double('joker', 'small'),
+			 [
+				Tractor(2, 2, Card('s', '8').suit_power(trump_card), SUIT_TRUMP),				 
+			 ]),
+		]
+
+		for test in tests:
+			test_cards, want = test
+			self.assertEqual(cards_to_tractors(test_cards, trick_suit, trump_card), want)
 
 	def testCardsToTractorsWithForm(self):
 		# trick suit always 'd', trump suit always 's'
@@ -119,17 +215,6 @@ class TestTractor(unittest.TestCase):
 			lesser_flush = Flush(cards_to_tractors(lesser_cards, trick_suit, trump_card))
 			greater_flush = Flush(cards_to_tractors(greater_cards, trick_suit, trump_card))
 			self.assertLess(lesser_flush, greater_flush)
-	
-	def testTractorValidity(self):
-		testInvalid = []
-
-		for test in testInvalid:
-			self.assertFalse(isTractorValid(test))
-
-		testValid = []
-
-		for test in testValid:
-			self.assertTrue(isTractorValid(test))
 
 if __name__ == '__main__':
 	unittest.main()
